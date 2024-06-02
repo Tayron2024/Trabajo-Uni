b2aef120c0771bcb87a0593a2699c159d923a75b
Tayron Morales <mtayronfernando@gmail.com>
2024-06-02 10:24:12 -0600

    Update 4.0.RadicacionRestas-For.c

--- 4.0.RadicacionRestas-For.c
+++ 4.0.RadicacionRestas-For.c
@@ -1,14 +1,4 @@
-/**
- * @file 3.sumasPotencia.c
- * @author Tayron Morales (mtayronfernando@gmail.com)
- * @brief 
- * @version 0.1
- * @date 2024-05-31
- * 
- * @copyright Copyright (c) 2024
- * 
- */
-/*
+
 4.Radicación mediante restas sucesivas
 Descripción: Calcular la raíz cúbica de un número utilizando restas sucesivas.
 Ejemplo: 27^(1/3) = 3
